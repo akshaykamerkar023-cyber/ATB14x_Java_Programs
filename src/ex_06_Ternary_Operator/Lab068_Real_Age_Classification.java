479f15621621deb44e9ca42676b3e67b710a474e
Akshay <akshaykamerkar023@gmail.com>
2025-09-29 21:17:49 +0530

    ex_06_Ternary_Operator

---
+++ src/ex_06_Ternary_Operator/Lab068_Real_Age_Classification.java
@@ -0,0 +1,18 @@
+package ex_06_Ternary_Operator;
+
+public class Lab068_Real_Age_Classification {
+    public static void main(String[] akky_args)  {
+
+        String user_input = akky_args[0];
+        System.out.println(user_input);
+        System.out.println(user_input instanceof String);
+
+
+        int age = Integer.parseInt(user_input);
+
+        String result = (age < 18) ? "Minor" : (age <= 60) ? "Adult" : "Sr.Citizen";
+        System.out.println(result);
+
+
+    }
+}
