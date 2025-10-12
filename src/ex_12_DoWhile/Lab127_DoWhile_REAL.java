482b50db9e6fd46671b5574e846398c0998d68a7
Akshay <akshaykamerkar023@gmail.com>
2025-10-12 20:06:35 +0530

    ex_12_Do_While

---
+++ src/ex_12_DoWhile/Lab127_DoWhile_REAL.java
@@ -0,0 +1,11 @@
+package ex_12_DoWhile;
+
+public class Lab127_DoWhile_REAL {
+    public static void main(String[] args) {
+        int number = 0;
+        do {
+            System.out.print(number);
+            number++;
+        }  while(number<0);
+    }
+}
