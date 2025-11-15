8206e90c2734c1ceb412aaae753073aa3b344fed
Akshay <akshaykamerkar023@gmail.com>
2025-11-15 22:10:41 +0530

    ex_19_OOPs_Part2 realExample

---
+++ src/singleinheritance/CommonTOAllTest.java
@@ -0,0 +1,19 @@
+package singleinheritance;
+
+public class CommonTOAllTest {
+    void startBrowser() {
+        System.out.println("Starting Browser");
+    }
+
+    void closeBrowser() {
+        System.out.println("Closing Browser");
+    }
+
+    void readExternal() {
+        System.out.println("Reading External Data");
+    }
+
+    void readDataBaseFile() {
+        System.out.println("Reading Data Base File");
+    }
+}
