3c942a6dd77ef7b491e87935212c09fd7401d0a8
Akshay <akshaykamerkar023@gmail.com>
2025-12-29 21:27:49 +0530

    ex_28_Object

---
+++ src/ex_28_Object/LabSample.java
@@ -0,0 +1,32 @@
+package ex_28_Object;
+
+    public class LabSample {
+      public LabSample() {
+            super();
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+    }
